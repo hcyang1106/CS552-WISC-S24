5414bf5a723dd8615a80f18a8c8d202e2940dee6
howard <howard.yang@kiwigame.com>
2024-03-01 08:58:50 -0600

    add PCS, HLT instr

--- single-cycle-cpu/code/Control.v
+++ single-cycle-cpu/code/Control.v
@@ -1,7 +1,8 @@
-module Control(opcode, MemWrite, MemToReg, RegWrite, BInv, ALUSrc, ALUOp, ShiftImm, ReadReg1Src, ReadReg2Src);
+module Control(opcode, MemWrite, WriteRegSrc, RegWrite, BInv, ALUSrc, ALUOp, ShiftImm, ReadReg1Src, ReadReg2Src, Halt);
     
     input [3:0] opcode;
-    output reg MemWrite, MemToReg, RegWrite, BInv, ShiftImm, ReadReg1Src, ReadReg2Src;
+    output reg MemWrite, RegWrite, BInv, ShiftImm, ReadReg1Src, ReadReg2Src, Halt;
+    output reg [1:0] WriteRegSrc;
     output reg [1:0] ALUSrc;
     output reg [3:0] ALUOp;
 
@@ -9,79 +10,86 @@ module Control(opcode, MemWrite, MemToReg, RegWrite, BInv, ALUSrc, ALUOp, ShiftI
 		case(opcode)
 			4'b0000: begin //ADD
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b00;
 				ALUOp			<= 4'b0000;
                 ReadReg1Src     <= 1'b0;
                 ReadReg2Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ShiftImm don't care 
 			end
 			4'b0001: begin // SUB
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b1;
                 ALUSrc          <= 2'b00;
 				ALUOp			<= 4'b0000;
                 ReadReg1Src     <= 1'b0;
                 ReadReg2Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ShiftImm don't care 
 			end
 			4'b0010: begin // XOR
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b00;
 				ALUOp			<= 4'b0001;
                 ReadReg1Src     <= 1'b0;
                 ReadReg2Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ShiftImm don't care
 			end
 			4'b0011: begin // RED
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b00;
 				ALUOp			<= 4'b0010;
                 ReadReg1Src     <= 1'b0;
                 ReadReg2Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ShiftImm don't care	
 			end
             4'b0100: begin // SLL
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b01;
 				ALUOp			<= 4'b0011;
                 ShiftImm        <= 1'b0;
                 ReadReg1Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ReadReg2Src don't care   
 			end
             4'b0101: begin // SRA
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b01;
 				ALUOp			<= 4'b0100;
                 ShiftImm        <= 1'b0;
                 ReadReg1Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ReadReg2Src don't care 
 			end
             4'b0110: begin // ROR
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b01;
 				ALUOp			<= 4'b0101;
                 ShiftImm        <= 1'b0;
                 ReadReg1Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ReadReg2Src don't care 
 			end
             // 4'b0010: begin // PADDSB
@@ -89,35 +97,38 @@ module Control(opcode, MemWrite, MemToReg, RegWrite, BInv, ALUSrc, ALUOp, ShiftI
 			// end
             4'b1010: begin // LLB
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b10;
 				ALUOp			<= 4'b0111;
                 ReadReg1Src     <= 1'b1;
+                Halt            <= 1'b0;
                 // ShiftImm don't care
                 // ReadReg2Src don't care
 			end
             4'b1011: begin // LHB
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b0;
+                WriteRegSrc     <= 2'b00;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b10;
 				ALUOp			<= 4'b1000;
                 ReadReg1Src     <= 1'b1;
+                Halt            <= 1'b0;
                 // ShiftImm don't care
                 // ReadReg2Src don't care
 			end
             4'b1000: begin // LW
 				MemWrite		<= 1'b0;
-                MemToReg        <= 1'b1;
+                WriteRegSrc     <= 2'b01;
                 RegWrite		<= 1'b1;
                 BInv            <= 1'b0;
                 ALUSrc          <= 2'b01;
 				ALUOp			<= 4'b0000;
                 ShiftImm        <= 1'b1;
                 ReadReg1Src     <= 1'b0;
+                Halt            <= 1'b0;
                 // ReadReg2Src don't care
 			end
             4'b1001: begin // SW
@@ -129,11 +140,37 @@ module Control(opcode, MemWrite, MemToReg, RegWrite, BInv, ALUSrc, ALUOp, ShiftI
                 ShiftImm        <= 1'b1;
                 ReadReg1Src     <= 1'b0;
                 ReadReg2Src     <= 1'b1;
-                // MemToReg don't care
+                Halt            <= 1'b0;
+                // WriteRegSrc don't care
+			end
+            4'b1110: begin // PCS
+				MemWrite		<= 1'b0;
+                RegWrite		<= 1'b1;
+                Halt            <= 1'b0;
+                WriteRegSrc     <= 2'b10;
+                // BInv            
+                // ALUSrc          
+				// ALUOp			
+                // ShiftImm       
+                // ReadReg1Src    
+                // ReadReg2Src  
+                // don't care
+			end
+            4'b1111: begin // HLT
+				MemWrite		<= 1'b0;
+                RegWrite		<= 1'b0;
+                Halt            <= 1'b1;
+                // BInv            
+                // ALUSrc          
+				// ALUOp			
+                // ShiftImm       
+                // ReadReg1Src    
+                // ReadReg2Src  
+                // WriteRegSrc don't care
 			end
             default: begin
             MemWrite        <= 1'b0;
-            MemToReg        <= 1'b0;
+            WriteRegSrc     <= 2'b00;
             RegWrite        <= 1'b0;
             BInv            <= 1'b0;
             ALUSrc          <= 2'b00;
@@ -141,6 +178,7 @@ module Control(opcode, MemWrite, MemToReg, RegWrite, BInv, ALUSrc, ALUOp, ShiftI
             ShiftImm        <= 1'b0;
             ReadReg1Src     <= 1'b0;
 	        ReadReg2Src     <= 1'b0;
+            Halt            <= 1'b0;
         end
 		endcase
 	end
--- single-cycle-cpu/code/cpu.v
+++ single-cycle-cpu/code/cpu.v
@@ -6,27 +6,27 @@ module cpu(clk, rst_n, hlt, pc);
 
     // component outputs
     wire [15:0] pc_out, addpc_out, im_out, dm_out, rf_out_1, rf_out_2, alu_out, 
-                memtoreg_mux_out, signext4_out, signext8_out, shift_out, shiftimm_mux_out,
+                writeregsrc_mux_out, signext4_out, signext8_out, shift_out, shiftimm_mux_out,
                 alusrc_mux_out, unused_16b_in;
     wire [3:0] readreg1src_mux_out, readreg2src_mux_out;
     wire V, addpc_ovfl; // V represents overflow, addpc_ovfl unused
 
     // control signals
-    wire [1:0] ALUSrc;
+    wire [1:0] ALUSrc, WriteRegSrc;
     wire [3:0] ALUOp;
-    wire MemWrite, MemtoReg, BInv, RegWrite, ShiftImm, ReadReg1Src, ReadReg2Src;
+    wire MemWrite, BInv, RegWrite, ShiftImm, ReadReg1Src, ReadReg2Src, Halt;
 
     // fixed setting
     wire [15:0] word_len = 16'h2;
-    wire pc_wen = 1'b1, addpc_sub = 1'b0, im_wen = 1'b0;
+    wire addpc_sub = 1'b0, im_wen = 1'b0;
 
-    PC PC(.in(addpc_out), .wen(pc_wen), .clk(clk), .rst(~rst_n), .out(pc_out));
+    PC PC(.in(addpc_out), .wen(~Halt), .clk(clk), .rst(~rst_n), .out(pc_out));
     Add AddPC(.a(pc_out), .b(word_len), .sub(addpc_sub), .sum(addpc_out), .ovfl(addpc_ovfl));
-    Control Control(.opcode(im_out[15:12]), .MemWrite(MemWrite), .MemToReg(MemtoReg), .RegWrite(RegWrite), .BInv(BInv), .ALUSrc(ALUSrc), .ALUOp(ALUOp), .ShiftImm(ShiftImm), .ReadReg1Src(ReadReg1Src), .ReadReg2Src(ReadReg2Src));
+    Control Control(.opcode(im_out[15:12]), .MemWrite(MemWrite), .WriteRegSrc(WriteRegSrc), .RegWrite(RegWrite), .BInv(BInv), .ALUSrc(ALUSrc), .ALUOp(ALUOp), .ShiftImm(ShiftImm), .ReadReg1Src(ReadReg1Src), .ReadReg2Src(ReadReg2Src), .Halt(Halt));
     IM IM(.data_out(im_out), .data_in(unused_16b_in), .addr(pc_out), .enable(rst_n), .wr(im_wen), .clk(clk), .rst(~rst_n));
     Mux2to1 #(4) ReadReg1SrcMux(.in0(im_out[7:4]), .in1(im_out[11:8]), .sel(ReadReg1Src), .out(readreg1src_mux_out));
     Mux2to1 #(4) ReadReg2SrcMux(.in0(im_out[3:0]), .in1(im_out[11:8]), .sel(ReadReg2Src), .out(readreg2src_mux_out));
-    RegFile RegFile(.clk(clk), .rst(~rst_n), .SrcReg1(readreg1src_mux_out), .SrcReg2(readreg2src_mux_out), .DstReg(im_out[11:8]), .WriteReg(RegWrite), .DstData(memtoreg_mux_out), .SrcData1(rf_out_1), .SrcData2(rf_out_2));
+    RegFile RegFile(.clk(clk), .rst(~rst_n), .SrcReg1(readreg1src_mux_out), .SrcReg2(readreg2src_mux_out), .DstReg(im_out[11:8]), .WriteReg(RegWrite), .DstData(writeregsrc_mux_out), .SrcData1(rf_out_1), .SrcData2(rf_out_2));
     SignExt #(4) SignExt4(.in(im_out[3:0]), .out(signext4_out));
     Shift Shift(.in(signext4_out), .out(shift_out));
     Mux2to1 ShiftImmMux(.in0(signext4_out), .in1(shift_out), .sel(ShiftImm), .out(shiftimm_mux_out));
@@ -34,6 +34,6 @@ module cpu(clk, rst_n, hlt, pc);
     Mux4to1 ALUSrcMux(.in0(rf_out_2), .in1(shiftimm_mux_out), .in2(signext8_out), .in3(unused_16b_in), .sel(ALUSrc), .out(alusrc_mux_out));
     ALU ALU(.a(rf_out_1), .b(alusrc_mux_out), .BInv(BInv), .ALUOp(ALUOp), .out(alu_out), .v(V));
     DM DM(.data_out(dm_out), .data_in(rf_out_2), .addr(alu_out), .enable(rst_n), .wr(MemWrite), .clk(clk), .rst(~rst_n));
-    Mux2to1 MemtoRegMux(.in0(alu_out), .in1(dm_out), .sel(MemtoReg), .out(memtoreg_mux_out));
+    Mux4to1 WriteRegSrcMux(.in0(alu_out), .in1(dm_out), .in2(addpc_out), .in3(unused_16b_in), .sel(WriteRegSrc), .out(writeregsrc_mux_out));
 
 endmodule
