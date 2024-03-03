11f33871d07ef08326e3a8fd60269a7df5039cd5
howard <howard.yang@kiwigame.com>
2024-03-03 01:49:23 -0600

    fix cla adder

--- single-cycle-cpu/code/Add.v
+++ single-cycle-cpu/code/Add.v
@@ -18,21 +18,21 @@ module Add16(a, b, sub, sum, ovfl);
     
     assign C[0] = sub;
     assign C[1] = G[0] | (P[0] & C[0]);
-    assign C[2] = G[1] | (P[1] & C[1]);
-    assign C[3] = G[2] | (P[2] & C[2]);
-    assign C[4] = G[3] | (P[3] & C[3]);
-    assign C[5] = G[4] | (P[4] & C[4]);
-    assign C[6] = G[5] | (P[5] & C[5]);
-    assign C[7] = G[6] | (P[6] & C[6]);
-    assign C[8] = G[7] | (P[7] & C[7]);
-    assign C[9] = G[8] | (P[8] & C[8]);
-    assign C[10] = G[9] | (P[9] & C[9]);
-    assign C[11] = G[10] | (P[10] & C[10]);
-    assign C[12] = G[11] | (P[11] & C[11]);
-    assign C[13] = G[12] | (P[12] & C[12]);
-    assign C[14] = G[13] | (P[13] & C[13]);
-    assign C[15] = G[14] | (P[14] & C[14]);
-    assign C[16] = G[15] | (P[15] & C[15]);
+    assign C[2] = G[1] | (P[1] & G[0]) | (&P[1:0] & C[0]);
+    assign C[3] = G[2] | (P[2] & G[1]) | (&P[2:1] & G[0]) | (&P[2:0] & C[0]);
+    assign C[4] = G[3] | (P[3] & G[2]) | (&P[3:2] & G[1]) | (&P[3:1] & G[0]) | (&P[3:0] & C[0]);
+    assign C[5] = G[4] | (P[4] & G[3]) | (&P[4:3] & G[2]) | (&P[4:2] & G[1]) | (&P[4:1] & G[0]) | (&P[4:0] & C[0]);
+    assign C[6] = G[5] | (P[5] & G[4]) | (&P[5:4] & G[3]) | (&P[5:3] & G[2]) | (&P[5:2] & G[1]) | (&P[5:1] & G[0]) | (&P[5:0] & C[0]);
+    assign C[7] = G[6] | (P[6] & G[5]) | (&P[6:5] & G[4]) | (&P[6:4] & G[3]) | (&P[6:3] & G[2]) | (&P[6:2] & G[1]) | (&P[6:1] & G[0]) | (&P[6:0] & C[0]);
+    assign C[8] = G[7] | (P[7] & G[6]) | (&P[7:6] & G[5]) | (&P[7:5] & G[4]) | (&P[7:4] & G[3]) | (&P[7:3] & G[2]) | (&P[7:2] & G[1]) | (&P[7:1] & G[0]) | (&P[7:0] & C[0]);
+    assign C[9] = G[8] | (P[8] & G[7]) | (&P[8:7] & G[6]) | (&P[8:6] & G[5]) | (&P[8:5] & G[4]) | (&P[8:4] & G[3]) | (&P[8:3] & G[2]) | (&P[8:2] & G[1]) | (&P[8:1] & G[0]) | (&P[8:0] & C[0]);
+    assign C[10] = G[9] | (P[9] & G[8]) | (&P[9:8] & G[7]) | (&P[9:7] & G[6]) | (&P[9:6] & G[5]) | (&P[9:5] & G[4]) | (&P[9:4] & G[3]) | (&P[9:3] & G[2]) | (&P[9:2] & G[1]) | (&P[9:1] & G[0]) | (&P[9:0] & C[0]);
+    assign C[11] = G[10] | (P[10] & G[9]) | (&P[10:9] & G[8]) | (&P[10:8] & G[7]) | (&P[10:7] & G[6]) | (&P[10:6] & G[5]) | (&P[10:5] & G[4]) | (&P[10:4] & G[3]) | (&P[10:3] & G[2]) | (&P[10:2] & G[1]) | (&P[10:1] & G[0]) | (&P[10:0] & C[0]);
+    assign C[12] = G[11] | (P[11] & G[10]) | (&P[11:10] & G[9]) | (&P[11:9] & G[8]) | (&P[11:8] & G[7]) | (&P[11:7] & G[6]) | (&P[11:6] & G[5]) | (&P[11:5] & G[4]) | (&P[11:4] & G[3]) | (&P[11:3] & G[2]) | (&P[11:2] & G[1]) | (&P[11:1] & G[0]) | (&P[11:0] & C[0]);
+    assign C[13] = G[12] | (P[12] & G[11]) | (&P[12:11] & G[10]) | (&P[12:10] & G[9]) | (&P[12:9] & G[8]) | (&P[12:8] & G[7]) | (&P[12:7] & G[6]) | (&P[12:6] & G[5]) | (&P[12:5] & G[4]) | (&P[12:4] & G[3]) | (&P[12:3] & G[2]) | (&P[12:2] & G[1]) | (&P[12:1] & G[0]) | (&P[12:0] & C[0]);
+    assign C[14] = G[13] | (P[13] & G[12]) | (&P[13:12] & G[11]) | (&P[13:11] & G[10]) | (&P[13:10] & G[9]) | (&P[13:9] & G[8]) | (&P[13:8] & G[7]) | (&P[13:7] & G[6]) | (&P[13:6] & G[5]) | (&P[13:5] & G[4]) | (&P[13:4] & G[3]) | (&P[13:3] & G[2]) | (&P[13:2] & G[1]) | (&P[13:1] & G[0]) | (&P[13:0] & C[0]);
+    assign C[15] = G[14] | (P[14] & G[13]) | (&P[14:13] & G[12]) | (&P[14:12] & G[11]) | (&P[14:11] & G[10]) | (&P[14:10] & G[9]) | (&P[14:9] & G[8]) | (&P[14:8] & G[7]) | (&P[14:7] & G[6]) | (&P[14:6] & G[5]) | (&P[14:5] & G[4]) | (&P[14:4] & G[3]) | (&P[14:3] & G[2]) | (&P[14:2] & G[1]) | (&P[14:1] & G[0]) | (&P[14:0] & C[0]);
+    assign C[16] = G[15] | (P[15] & G[14]) | (&P[15:14] & G[13]) | (&P[15:13] & G[12]) | (&P[15:12] & G[11]) | (&P[15:11] & G[10]) | (&P[15:10] & G[9]) | (&P[15:9] & G[8]) | (&P[15:8] & G[7]) | (&P[15:7] & G[6]) | (&P[15:6] & G[5]) | (&P[15:5] & G[4]) | (&P[15:4] & G[3]) | (&P[15:3] & G[2]) | (&P[15:2] & G[1]) | (&P[15:1] & G[0]) | (&P[15:0] & C[0]);
 
     assign raw_sum = a ^ _b ^ C[15:0];
     assign ovfl = C[16] ^ C[15];
@@ -62,9 +62,9 @@ module Add4(a, b, sub, sum, ovfl);
     
     assign C[0] = sub;
     assign C[1] = G[0] | (P[0] & C[0]);
-    assign C[2] = G[1] | (P[1] & C[1]);
-    assign C[3] = G[2] | (P[2] & C[2]);
-    assign C[4] = G[3] | (P[3] & C[3]);
+    assign C[2] = G[1] | (P[1] & G[0]) | (&P[1:0] & C[0]);
+    assign C[3] = G[2] | (P[2] & G[1]) | (&P[2:1] & G[0]) | (&P[2:0] & C[0]);
+    assign C[4] = G[3] | (P[3] & G[2]) | (&P[3:2] & G[1]) | (&P[3:1] & G[0]) | (&P[3:0] & C[0]);
 
     assign raw_sum = a ^ _b ^ C[3:0];
     assign ovfl = C[4] ^ C[3];
@@ -73,44 +73,3 @@ module Add4(a, b, sub, sum, ovfl);
                  raw_sum; // No overflow
 
 endmodule
-
-// module Add(a, b, sub, sum, ovfl);
-
-//     // Should be modified to a CLA adder, this is used temporarily
-
-//     input [15:0] a;
-//     input [15:0] b;
-//     input sub;
-//     output [15:0] sum;
-//     output ovfl;
-
-//     wire [15:0] _b = sub ? ~b : b;
-//     wire cin0 = sub;
-//     wire cin1, cin2, cin3, cin4,
-//         cin5, cin6, cin7, cin8,
-//         cin9, cin10, cin11, cin12,
-//         cin13, cin14, cin15, cin16;
-
-//     FullAdder FA0(.a(a[0]), .b(_b[0]), .cin(cin0), .sum(sum[0]), .cout(cin1));
-//     FullAdder FA1(.a(a[1]), .b(_b[1]), .cin(cin1), .sum(sum[1]), .cout(cin2));
-//     FullAdder FA2(.a(a[2]), .b(_b[2]), .cin(cin2), .sum(sum[2]), .cout(cin3));
-//     FullAdder FA3(.a(a[3]), .b(_b[3]), .cin(cin3), .sum(sum[3]), .cout(cin4));
-
-//     FullAdder FA4(.a(a[4]), .b(_b[4]), .cin(cin4), .sum(sum[4]), .cout(cin5));
-//     FullAdder FA5(.a(a[5]), .b(_b[5]), .cin(cin5), .sum(sum[5]), .cout(cin6));
-//     FullAdder FA6(.a(a[6]), .b(_b[6]), .cin(cin6), .sum(sum[6]), .cout(cin7));
-//     FullAdder FA7(.a(a[7]), .b(_b[7]), .cin(cin7), .sum(sum[7]), .cout(cin8));
-
-//     FullAdder FA8(.a(a[8]), .b(_b[8]), .cin(cin8), .sum(sum[8]), .cout(cin9));
-//     FullAdder FA9(.a(a[9]), .b(_b[9]), .cin(cin9), .sum(sum[9]), .cout(cin10));
-//     FullAdder FA10(.a(a[10]), .b(_b[10]), .cin(cin10), .sum(sum[10]), .cout(cin11));
-//     FullAdder FA11(.a(a[11]), .b(_b[11]), .cin(cin11), .sum(sum[11]), .cout(cin12));
-
-//     FullAdder FA12(.a(a[12]), .b(_b[12]), .cin(cin12), .sum(sum[12]), .cout(cin13));
-//     FullAdder FA13(.a(a[13]), .b(_b[13]), .cin(cin13), .sum(sum[13]), .cout(cin14));
-//     FullAdder FA14(.a(a[14]), .b(_b[14]), .cin(cin14), .sum(sum[14]), .cout(cin15));
-//     FullAdder FA15(.a(a[15]), .b(_b[15]), .cin(cin15), .sum(sum[15]), .cout(cin16));
-    
-//     assign ovfl = cin15 ^ cin16;
-
-// endmodule
