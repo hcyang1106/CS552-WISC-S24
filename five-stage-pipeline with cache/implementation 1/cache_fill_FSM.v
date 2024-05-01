69fcaad1f59a657bd8bd1247cbac8dfa2509a4cf
howard <howard.yang@kiwigame.com>
2024-05-01 13:30:07 -0500

    fix cache_fill_FSM

--- five-stage-pipeline with cache/implementation 1/cache_fill_FSM.v
+++ five-stage-pipeline with cache/implementation 1/cache_fill_FSM.v
@@ -1,50 +1,42 @@
-// NOT TESTED
-
 module cache_fill_FSM(clk, rst, miss_detected, miss_address, fsm_busy, write_data_array, 
-        write_tag_array, memory_address, memory_data_valid);
-    input clk, rst;          
+                      write_tag_array, memory_address, memory_data_valid);
+    input clk, rst;           
     input miss_detected;        
     input memory_data_valid;    
     input[15:0] miss_address;   
     output reg fsm_busy;        
+    output[15:0] memory_address;
     output reg write_tag_array; 
     output reg write_data_array;
-    output[15:0] memory_address;
 
-    wire state;                 // Output of state flop
-    wire nxtState;              // Input of state flop
-    wire[3:0] nxtCount, count;  // Input and Output of counter flop
-    wire[3:0] countUp;          // count + 1
-    wire cnt_full;              // high when count == 8
-    wire[11:0] block_address;   // first 12 bits of missing address
-    wire cnt_en;                // enable signal for counter flop
+    wire state;                 
+    reg next_state;               
+    wire[3:0] next_count, count;  
+    wire[3:0] count_up_one;  
+    reg count_en;            
+    wire count_full;              
+    wire[11:0] block_address;   
+                 
+
+    dff state_flip_flop(.clk(clk), .rst(rst), .d(next_state), .q(state), .wen(1'b1));
     
-    // State encodings
-    parameter IDLE = 1'b0;
-    parameter WAIT = 1'b1;
-
-    // 0: IDLE, 1: WAIT
-    dff stateFlop(.clk(clk), .rst(rst), .d(nxtState), .q(state), .wen(1'b1));
+    dff counter[3:0](.clk(clk), .rst(rst), .d(next_count[3:0]), .q(count[3:0]), .wen(count_en));
+    inc_3bit inc_count_one(.in(count[2:0]), .out(count_up_one[2:0]), .cout(count_up_one[3])); 
+    assign next_count = (state & count_full) ? 4'b0000 : count_up_one; 
+    assign count_full = count[3]; 
     
-    // counter counts from 1 to 8, cnt_full = 1 when it current count is 8
-    dff counter[3:0](.clk(clk), .rst(rst), .d(nxtCount[3:0]), .q(count[3:0]), .wen(cnt_en));
-    inc_3bit inc_count(.in(count[2:0]), .out(countUp[2:0]), .cout(countUp[3])); 
-    assign nxtCount = countUp;
-    assign cnt_full = count[3];
-
-    // cnt_en logic
-    assign cnt_en =  (state == WAIT) & memory_data_valid & !cnt_full;
-    // nxtState logic
-	assign nxtState = (state == WAIT) & !cnt_full;
-    // fsm_busy logic
-    assign fsm_busy = (state == WAIT) | (miss_detected & state == IDLE); 
-    // write_tag_array logic
-    assign write_tag_array = (state == WAIT) & cnt_full;
-    // write_data_array logic
-    assign write_data_array = (state == WAIT) & memory_data_valid;
-
-    assign block_address = miss_address[15:4];                                 
-    assign memory_address = {block_address, count[2:0], 1'b0};
+    assign block_address = miss_detected ? miss_address[15:4] : block_address;                                   
+    assign memory_address = fsm_busy ? {block_address, count[2:0], 1'b0} : miss_address;
+          
+    always @ (state, miss_detected, memory_data_valid, count_full)
+        casex ({state, miss_detected, memory_data_valid, count_full}) 
+            4'b0_0_?_?: begin next_state = 0; fsm_busy = 0; write_tag_array = 0; write_data_array = 0; count_en = 0; end
+            4'b0_1_?_?: begin next_state = 1; fsm_busy = 1; write_tag_array = 0; write_data_array = 0; count_en = 0; end
+            4'b1_?_?_1: begin next_state = 0; fsm_busy = 1; write_tag_array = 1; write_data_array = 0; count_en = 1; end
+            4'b1_?_0_0: begin next_state = 1; fsm_busy = 1; write_tag_array = 0; write_data_array = 0; count_en = 0; end
+            4'b1_?_1_0: begin next_state = 1; fsm_busy = 1; write_tag_array = 0; write_data_array = 1; count_en = 1; end   
+            default: begin next_state = 0; fsm_busy = 0; write_tag_array = 0; write_data_array = 0; count_en = 0; end
+        endcase
       
 endmodule
 
@@ -55,16 +47,14 @@ module inc_3bit(in, out, cout);
     
     wire int1, int2;
     
-    // 1st bit
     assign out[0] = in[0] ^ 1; 
     assign int1 = in[0] & 1;
     
-    // 2nd bit
     assign out[1] = in[1] ^ int1;
     assign int2 = in[1] & int1;
     
-    // 3rd bit
     assign out[2] = in[2] ^ int2;
 	assign cout = in[2] & int2;
     
 endmodule
+
